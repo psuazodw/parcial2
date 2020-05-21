d38bf04da61a765067757b464958642eda187a60
PSuazoJr <psuazodw@gmail.com>
2020-05-20 19:55:05 -0600

    Agregando la suma de num1 + num2

---
+++ parcial2.java
@@ -0,0 +1,11 @@
+public class Parcial2 {
+
+	public static void main (String[] args){
+
+		System.out.println("Hola mundo");
+		int num1 = 3;
+		int num2 = 6;
+		System.out.println(num1 + num2);
+
+	}
+}
